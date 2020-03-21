45bd333caaaf4e1667249e9ee9b0f47689af7918
CK <carl.klein@saxsys.de>
2020-03-21 19:50:40 +0100

    Reduce protractor browserstack config to a minimum

--- protractor.browserstack.conf.js
+++ protractor.browserstack.conf.js
@@ -1,10 +1,7 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 const browserstack = require('browserstack-local');
-const fs = require('fs');
-const path = require('path');
-const useragent = require('useragent');
 
-const commonPropertiesForCapabilities = {
+const browserstackCapabilities = {
   project: 'Tour of Heroes Browserstack Test',
   'browserstack.local': 'true',
   'browserstack.timezone': 'UTC',
@@ -19,10 +16,8 @@ exports.config = {
   allScriptsTimeout: 300000,
   getPageTimeout: 30000,
   specs: ['./e2e/specs/*.e2e-spec.ts'],
-
-  // bs-local.com is a browserstack alias that redirects to the locally hosted instance
-  baseUrl: 'http://bs-local.com:4000/',
   seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
+  maxSessions: Number(process.env.BROWSERSTACK_SESSION_COUNT || 2),
 
   multiCapabilities: [
     {
@@ -32,83 +27,44 @@ exports.config = {
       browserName: 'Chrome',
       browser_version: '76.0',
       resolution: '1920x1080',
-      ...commonPropertiesForCapabilities
+      ...browserstackCapabilities
     }
   ],
-  // values greater than 2 can lead to connection problems during the tests
-  maxSessions: Number(process.env.BROWSERSTACK_SESSION_COUNT || 2),
 
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 300000,
+    defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  SELENIUM_PROMISE_MANAGER: false,
 
-  // beforeAll
   beforeLaunch() {
     return new Promise((resolve, reject) => {
       console.log('Connecting local');
       exports.bs_local = new browserstack.Local();
       exports.bs_local.start(
         {
-          key: commonPropertiesForCapabilities['browserstack.key']
+          key: browserstackCapabilities['browserstack.key']
         },
         function(error) {
           if (error) return reject(error);
           console.log('Connected. Now testing...');
-
           resolve();
         }
       );
     });
   },
 
-  // beforeEach capability
   async onPrepare() {
     require('ts-node').register({
-      project: __dirname + '/tsconfig.json'
+      project: 'e2e/tsconfig.e2e.json'
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
 
-    // statistics export configuration
-    const userAgent = useragent.parse(await browser.driver.executeScript('return navigator.userAgent'));
-    const parsedUserAgentForFileName = userAgent
-      .toString()
-      .replace(/\s/g, '_')
-      .replace(/\//g, '_')
-      .toLowerCase();
-    const outFile = `statistics/e2e-bs_${parsedUserAgentForFileName}.json`;
-    if (!fs.existsSync(path.dirname(outFile))) {
-      fs.mkdirSync(path.dirname(outFile));
-    }
-
-    await logSessionIdAndUserAgent();
-
-    return browser.getProcessedConfig().then(config => resizeWindow(config));
+    return browser.getProcessedConfig();
   },
 
-  // afterAll
   afterLaunch() {
     return new Promise(resolve => exports.bs_local.stop(resolve));
-  }
-};
-
-const logSessionIdAndUserAgent = async function() {
-  const sessionId = await browser.getSession().then(session => session.getId());
-  const userAgent = useragent.parse(await browser.driver.executeScript('return navigator.userAgent'));
-  console.log(`Browserstack session is running on ${userAgent} with id ${sessionId}`);
-};
-
-const resizeWindow = async function(config) {
-  const isDesktop = config.capabilities.os === 'Windows' || config.capabilities.os === 'OS X';
-  if (isDesktop === true) {
-    await browser.driver
-      .manage()
-      .window()
-      .maximize();
-    console.log('Maximized window');
-    browser.switchTo();
   }
 };
